d7e2a2e993d9458d4cdf983882db16b4e4b5c3ed
Nadiia <zoryvchaknv@gmail.com>
2024-10-09 14:44:32 +0300

    Fixed bugs and save changes in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
-const js = require("@eslint/js");
+const reactPlugin = require('eslint-plugin-react');
+const globals = require('globals');
 
 module.exports = [
-  js.configs.recommended,
   {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    }
-  }
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    ...reactPlugin.configs.flat.recommended, 
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
 ];
